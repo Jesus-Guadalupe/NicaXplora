e06599b2d1560b0e004f1a9dc8a6e6fdfd1a017e
Cristian <cristianzapata2007@gmail.com>
2025-09-29 21:47:48 -0600

    modificación logo navbar

--- frontend/src/pages/Destinos.jsx
+++ frontend/src/pages/Destinos.jsx
@@ -3,7 +3,6 @@ import Navbar from '../components/navbar'
 import { IoIosStar } from "react-icons/io";
 import { PiMapPinFill } from "react-icons/pi";
 import SliderLugares from '../components/DestinosComponents/SliderLugares';
-import Comentarios from '../components/DestinosComponents/Comentarios';
 
 
 const Destinos = () => {
